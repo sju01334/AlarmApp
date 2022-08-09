b9408ce4cd9b9cd72769e77b85af4755628fed3e
sju01334 <sju01334@gmail.com>
2022-08-09 18:47:20 +0900

    버튼 클리시 DataPicker 창 생성 및 선택시 갱신 sharedPreference 를 통한 시간 가져오기 PendingIntent 생성 BroadcastReceiver 클래서 생성

---
+++ app/src/main/java/com/nepplus/alarmapp/AlarmReceiver.kt
@@ -0,0 +1,22 @@
+package com.nepplus.alarmapp
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class AlarmReceiver : BroadcastReceiver(){
+    override fun onReceive(context: Context, intent: Intent) {
+        createNotificationChannel(context)
+        notifyNotification(context)
+
+    }
+
+    private fun createNotificationChannel(context: Context) {
+
+    }
+
+    private fun notifyNotification(context: Context) {
+
+    }
+
+}
